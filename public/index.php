4bcc6db3635cd048cce96ed06f834aaad5dcb08b
William Jacinto Venancio <wilmonjv@yahoo.com.br>
2018-02-26 00:05:05 -0300

    0.05b - Correção no PATH
    Agora o path se adapta a pastas internas

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__DIR__));
-define('PATH', '/'.basename(ROOT));
+define('PATH', str_replace($_SERVER["DOCUMENT_ROOT"],"",str_replace(array('/', '\\'),'/', ROOT)));
 
 require_once(ROOT.DS.'library'.DS.'loader.php');
